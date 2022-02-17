bdef3af934aa64bf2b5c759ab301066804da1993
Mikaal <matthewnaik@gmail.com>
2022-02-17 08:49:07 -0500

    Test GH action workflow for Heroku CD

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,7 +1,7 @@
 import { Db, MongoClient } from 'mongodb';
 
 const connectionString = process.env.MONGO_URL;
-
+console.log('connectionString', connectionString);
 const client = new MongoClient(connectionString);
 
 let dbConnection: Db;
