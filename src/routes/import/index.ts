efe4f901a6e1689408d8925fb6fefe2641d6de2f
Mikaal <matthewnaik@gmail.com>
2022-03-07 17:20:18 -0500

    lots of misc worek

--- src/routes/import/index.ts
+++ src/routes/import/index.ts
@@ -1,4 +1,8 @@
+import { WeatherStations } from 'consts/weatherStations';
+import dayjs from 'dayjs';
 import express from 'express';
+import { parseWeatherXML } from 'utils/backfill/weather';
+import { getWeather } from 'utils/backfill/weather/get-weather';
 import { importLatestOntarioPlaceReadings } from 'utils/import/ontario-place';
 import { importTorontoBeachReadings } from '../../utils/import/toronto-beaches';
 
@@ -15,6 +19,23 @@ router.get('/beaches', async (req, res) => {
   const results = await importLatestOntarioPlaceReadings();
   const torontoResults = await importTorontoBeachReadings();
   res.send({ results, torontoResults });
+});
+
+
+router.get('/weather', async (req, res) => {
+  const month = dayjs().month() + 1;
+  const year = dayjs().year();
+
+  const rawWeather = await getWeather(year, WeatherStations.PostJune2013);
+  const weather = await parseWeatherXML(rawWeather);
+
+  console.log('weather', weather);
+
+  res.send({ weather, month, year });
+
+
+
+
 });
 
 // Define the about route
