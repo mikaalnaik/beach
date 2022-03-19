50f7d4282ac5eabb77da5ce09ee201858cab10ae
Mikaal <matthewnaik@gmail.com>
2022-03-19 12:32:02 -0400

    Minor change to see if CI is happening with mono repo

--- apps/backend/src/routes/greetings.ts
+++ apps/backend/src/routes/greetings.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  res.send('Welcome to the Toronto Beach API! Check out the docs/code at https://github.com/mikaalnaik/beach-backend');
+  res.send('Welcome to the Toronto Beach API! Check out the code at https://github.com/mikaalnaik/beach-backend. Feel free to create a PR or an Issue there!');
 });
 
 
