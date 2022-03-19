58d3c31a063c5b53bf303fceb3884e75202d723b
Mikaal <matthewnaik@gmail.com>
2022-03-19 11:23:26 -0400

    Add a little text message to the root endpoint

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ import express from 'express';
 
 const app = express();
 
+app.use('/', require('./routes/greetings'));
 app.use('/beaches', require('./routes/beaches'));
 app.use('/historical', require('./routes/historical'));
-app.use('/greetings', require('./routes/greetings'));
 app.use('/backfill', require('./routes/backfill'));
 app.use('/status', require('./routes/status'));
 app.use('/import', require('./routes/import'));
--- src/routes/greetings.ts
+++ src/routes/greetings.ts
@@ -1,31 +1,9 @@
-import mongo from '../mongo';
 import express from 'express';
 
 const router = express.Router();
 
-router.use(function timeLog(req, res, next) {
-  console.log('Time: ', Date.now());
-  next();
-});
-
-
-// Define the home page route
 router.get('/', async (req, res) => {
-  const db = mongo.getDb();
-  const result = await db.collection('records')
-    .find({ beachReadings: { $exists: true }})
-    .sort({ _id: 1 })
-    .limit(10)
-    .toArray();
-
-  // TODO: standardize the response type from the mongo response
-
-  res.send(result);
-});
-
-// Define the about route
-router.get('/about', async (_, res) =>{
-  res.send('About us');
+  res.send('Welcome to the Toronto Beach API! Check out the docs/code at https://github.com/mikaalnaik/beach-backend');
 });
 
 
