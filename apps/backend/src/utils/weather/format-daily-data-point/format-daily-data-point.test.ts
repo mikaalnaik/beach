02672e648f699198ac5c88a1f87febe257c83486
Mikaal <matthewnaik@gmail.com>
2022-03-21 08:35:56 -0400

    Add eslint

--- apps/backend/src/utils/weather/format-daily-data-point/format-daily-data-point.test.ts
+++ apps/backend/src/utils/weather/format-daily-data-point/format-daily-data-point.test.ts
@@ -1,6 +1,7 @@
 
-
+// TODO: add unit tests!
 describe('Format Weather Point', () => {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   it('produces the correct date format', () => {
 
   });
