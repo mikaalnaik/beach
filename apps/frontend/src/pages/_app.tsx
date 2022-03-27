97796b4a542e94bebd007176bde4528c6609eccc
Mikaal <matthewnaik@gmail.com>
2022-03-27 12:27:00 -0400

    Add Google analytics tagging

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,24 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import '../styles/app.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const handleRouteChange = url => {
+    window.gtag('config', 'G-LC1XE1N6HK', {
+      page_path: url,
+    });
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+
   return <Component {...pageProps} />;
 }
