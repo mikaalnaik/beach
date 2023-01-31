d56c8a80f3b0d8bd1f731629efdd66d24e16355f
Mikaal Naik <mikaalnaik@gmail.com>
2023-01-30 19:17:43 -0500

    Remove console logs and formatting

--- apps/frontend/src/pages/index.tsx
+++ apps/frontend/src/pages/index.tsx
@@ -10,13 +10,9 @@ import Head from 'next/head';
 import { HeavyRainFallAdvisory } from 'src/data/advisories';
 
 export async function getStaticProps() {
-  console.log('this is the getStaticProps');
   let beaches = {};
   try {
-
-    console.log('yeah', `${endpoint}/beaches/latest`);
     const beachResponse = await fetch(`${endpoint}/beaches/latest`);
-    console.log('beachResponse', beachResponse);
     beaches = await beachResponse.json();
   } catch (error) {
     console.log('error', error);
@@ -35,8 +31,9 @@ interface Props {
 }
 
 export default function Home({ beaches }: Props) {
-  console.log('beaches', beaches);
-  const hasAdvisory = beaches.some(beach => beach.advisory === HeavyRainFallAdvisory);
+  const hasAdvisory = beaches.some(
+    beach => beach.advisory === HeavyRainFallAdvisory
+  );
 
   const beachCards = beaches.map((beach, index) => (
     <BeachCard beach={beach} key={index} />
@@ -46,23 +43,21 @@ export default function Home({ beaches }: Props) {
     <Layout>
       <Head>
         <title>Toronto Beach Report</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="keywords" content="toronto beaches, water quality, beach ecoli"/>
-        <meta name="description" content="The easiest way to access information about Toronto's beaches e.Coli levels and safety standards"></meta>
+        <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+        <meta
+          name='keywords'
+          content='toronto beaches, water quality, beach ecoli'
+        />
+        <meta
+          name='description'
+          content="The easiest way to access information about Toronto's beaches e.Coli levels and safety standards"
+        ></meta>
       </Head>
       <section className={styles.description}>
-        <p>
-          E. coli measurements are per 100 ml of water.
-        </p>
-        {hasAdvisory && (
-          <p>
-            {HeavyRainFallAdvisory}
-          </p>
-        )}
-        <div className={styles['beach-list']}>
-          {beachCards}
-        </div>
+        <p>E. coli measurements are per 100 ml of water.</p>
+        {hasAdvisory && <p>{HeavyRainFallAdvisory}</p>}
+        <div className={styles['beach-list']}>{beachCards}</div>
       </section>
     </Layout>
   );
